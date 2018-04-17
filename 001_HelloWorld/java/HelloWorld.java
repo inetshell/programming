3fec99878956b94fc50f4bc666595e87739fe2eb
juanmanuel.carrillo.moreno <juanmanuel.carrillo.moreno@bbva.com>
2018-04-17 10:28:36 -0500

    upload HelloWorld.java

---
+++ 001_HelloWorld/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * How to compile:
+ * javac HelloWorld.java
+ *
+ * How to compile:
+ * java HelloWorld
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n");
+    }
+}
